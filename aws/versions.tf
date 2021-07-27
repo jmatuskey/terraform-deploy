bd12c6f7a5581b6fa3d407682caecb86c039f679
EC2 Default User <ec2-user@ip-10-142-24-122.ec2.internal>
2021-07-27 19:41:10 +0000

    Switch from instance_type = "" to instance_types = ["", ...] in nodegroups. Set default notebook instance_type to r5.xlarge. Upgrade pinned versions,  switch to supported autoscaler repo. Simplified configuration by computing rolearn and ditching allowed_roles. Update k8s version to 1.21 Test vs. Terraform-1.0.3 OK

--- aws/versions.tf
+++ aws/versions.tf
@@ -2,27 +2,39 @@ terraform {
    backend "s3" {  
      # backend file expected to fill in here, vars not allowed 
    }
-   required_version = "~> 0.14.5"
+   required_version = "~> 1.0.3"
    required_providers {
       aws = {
           source  = "hashicorp/aws"
-          version = "~> 3.26.0"
+          version = "~> 3.51.0"
       }
       template = {
           source  = "hashicorp/template"
-          version = "~> 2.1"
+          version = "~> 2.2.0"
       }
       kubernetes = {
           source  = "hashicorp/kubernetes"
-          version = "~> 1.11.1"
+          version = "~> 2.3.2"
+      }
+      cloudinit = {
+	  source = "hashicorp/cloudinit"
+	  version = "~> 2.2.0"
       }
       helm = {
           source  = "hashicorp/helm"
-          version = "~> 2.0.2"
+          version = "~> 2.2.0"
       }
       null = {
           source  = "hashicorp/null"
-          version = "~> 3.0.0"
+          version = "~> 3.1.0"
+      }
+      local = {
+          source = "hashicorp/local"
+          version = "~> 2.1.0"
+      }
+      http = {
+          source = "terraform-aws-modules/http"
+          version = "~> 2.4.1"
       }
    }
 }
@@ -35,7 +47,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  #load_config_file       = false
 }
 
 provider "helm" {
