f5bc839207d9368c7936d588dee281a576bd0b85
EC2 Default User <ec2-user@ip-10-144-24-177.ec2.internal>
2021-08-19 14:53:32 +0000

    add new monitoring module

---
+++ monitoring/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "rolename" {
+  description = "The name of the primary deployment role"
+  type = string
+}
+
+
+# -------------------------------------------------------------------------
+#                     Networking config
+
+variable vpc_name {
+   description = "Name of unmanaged VPC, e.g. created by IT department."
+   type = string
+}
+
+variable private_subnet_names {
+   description = "Patterns applied to Name tag to select unmanaged private subnets from the unmanaged vpc"
+   type = list(string)
+   default = ["*DMZ*"]
+}
+
+variable public_subnet_names {
+   description = "Patterns applied to Name tag to select unmanaged public subnets from the unmanaged vpc"
+   type = list(string)
+   default = ["*Public*"]
+}
